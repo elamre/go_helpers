151744c305bfec65937bfcf35e88819cb6883df3
elmar <elmarvanrijnswou@hotmail.com>
2022-04-11 21:47:50 +0200

    Added function to pass custom comparator

--- pkg/slice_helpers/helpers.go
+++ pkg/slice_helpers/helpers.go
@@ -8,7 +8,7 @@ func ListEquals[t comparable](listOne []t, listTwo []t) bool {
 		return true
 	}
 	if len(listOne) == 0 {
-		return false
+		return true
 	}
 	for i := 0; i < len(listOne); i++ {
 		if listOne[i] != listTwo[i] {
@@ -22,7 +22,10 @@ func RemoveFromList[t comparable](element t, list []t) []t {
 	if len(list) == 0 {
 		return nil
 	} else if len(list) == 1 {
-		return make([]t, 0)
+		if list[0] == element {
+			return make([]t, 0)
+		}
+		return nil
 	} else if list[0] == element {
 		return list[1:]
 	} else if list[len(list)-1] == element {
@@ -35,4 +38,32 @@ func RemoveFromList[t comparable](element t, list []t) []t {
 		}
 	}
 	return nil
+}
+
+func RemoveFromListEquals[t any](list []t, equals func(t) bool) []t {
+	if len(list) == 0 {
+		return nil
+	} else if len(list) == 1 {
+		return make([]t, 0)
+	}
+	var i int
+	var found bool
+	for i = 0; i < len(list); i++ {
+		if equals(list[i]) {
+			found = true
+			break
+		}
+	}
+	if !found {
+		return nil
+	}
+	if len(list) == 1 {
+		return make([]t, 0)
+	} else if i == 0 {
+		return list[1:]
+	} else if i == len(list)-1 {
+		return list[:len(list)-1]
+	}
+	list = append(list[:i], list[i+1:]...)
+	return list
 }
