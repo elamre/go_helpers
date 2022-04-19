47881cd79791e094e5d0e88357a5da11449ebad6
elamre <elmarvanrijnswou@hotmail.com>
2022-04-19 09:20:44 +0200

    Adding documentation, adding Cast function

--- pkg/misc/misc.go
+++ pkg/misc/misc.go
@@ -1,12 +1,21 @@
 package misc
 
+// CheckError checks the return of a function returning an error. Panics if error != nil
 func CheckError(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
+// CheckErrorRetVal checks the error value of a function returning an error and data
+// It's using generics for the return value type
 func CheckErrorRetVal[t any](value t, err error) t {
 	CheckError(err)
 	return value
+}
+
+// Cast function casts an interface to a type using generics.
+// returns obj.(t)
+func Cast[t any](obj interface{}) t {
+	return obj.(t)
 }
